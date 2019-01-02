ff9a097d493794f664fc46a4c7bfeff31540cef2
Mike <michael.schulman88@gmail.com>
2019-01-02 11:51:00 +0200

    fixing css/python

--- main.py
+++ main.py
@@ -13,21 +13,47 @@ def index():
     return template("about.html")
 
 
+# ----------------------------------------------------------------
+# JAVASCRIPT -----------------------------------------------------
+# ----------------------------------------------------------------
+
+@get('/vendor/bootstrap/js/<filename:re:.*\.js>')
+def js_bootstrap(filename):
+    return static_file(filename, root='vendor/bootstrap/js')
+
+
+@get('/vendor/jquery/<filename:re:.*\.js>')
+def js_jquery(filename):
+    return static_file(filename, root='vendor/jquery')
+
+
 @get('/js/<filename:re:.*\.js>')
-def javascripts(filename):
+def js_logic(filename):
     return static_file(filename, root='js')
 
 
-# ATTN: IS BELOW NEEDED?
-@get('/js/<filename:re:.*\.js>')
-def javascripts(filename):
+@get('/<filename:re:.*\.js>')
+def js_root(filename):
     return static_file(filename, root='')
 
 
-@get('/css/modern-business.css>')
-def stylesheets():
-    return static_file('modern-business.css', root='css')
+# ----------------------------------------------------------------
+# CSS ------------------------------------------------------------
+# ----------------------------------------------------------------
 
+@get('/vendor/bootstrap/css/<filename:re:.*\.css>')
+def stylesheets_bootstrap(filename):
+    return static_file(filename, root='vendor/bootstrap/css')
+
+
+@get('/css/<filename:re:.*\.css>')
+def stylesheets_css(filename):
+    return static_file(filename, root='css')
+
+
+# ----------------------------------------------------------------
+# OTHER ----------------------------------------------------------
+# ----------------------------------------------------------------
 
 @get('/images/<filename:re:.*\.(jpg|png|gif|ico)>')
 def images(filename):
